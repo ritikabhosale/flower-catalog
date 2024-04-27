fa2861d3d4f9f83e60bef99bc6b02d95cbde8fb6
Ritika Bhosale <54773240+ritikabhosale@users.noreply.github.com>
2024-04-27 10:01:25 +0530

    Commit with a breaking test case

--- test/testApp.js
+++ test/testApp.js
@@ -1,32 +1,34 @@
-const { createApp } = require('../src/app.js');
-const request = require('supertest');
-const assert = require('assert');
+const { createApp } = require("../src/app.js");
+const request = require("supertest");
+const assert = require("assert");
 
 const mockConsoleLog = (...expectedText) => {
   return (...actualText) => {
     assert.deepEqual(actualText, expectedText);
-  }
-}
+  };
+};
 
-describe('GET /abc', () => {
-  it('should route to not found on /abc ', (done) => {
-    const appConfig = { staticDir: './public' };
-    const mockedConsoleLog = mockConsoleLog('GET', '/abc');
+describe("GET /abc", () => {
+  it("should route to not found on /abc ", (done) => {
+    const appConfig = { staticDir: "./public" };
+    const mockedConsoleLog = mockConsoleLog("GET", "/abc");
     const req = request(createApp(appConfig, {}, {}, mockedConsoleLog));
-    req.get('/abc')
-      .expect(404, '/abc not Found', done)
-      .expect('content-type', /plain/)
+    req
+      .get("/abc")
+      .expect(404, "/abc not Found", done)
+      .expect("content-type", /plainn/);
   });
 });
 
-describe('GET /', () => {
-  it('should send static content', (done) => {
-    const appConfig = { staticDir: './public' };
-    const mockedConsoleLog = mockConsoleLog('GET', '/');
+describe("GET /", () => {
+  it("should send static content", (done) => {
+    const appConfig = { staticDir: "./public" };
+    const mockedConsoleLog = mockConsoleLog("GET", "/");
     const req = request(createApp(appConfig, {}, {}, mockedConsoleLog));
-    req.get('/')
+    req
+      .get("/")
       .expect(200, done)
-      .expect('content-type', /html/)
-      .expect('content-length', "835")
+      .expect("content-type", /html/)
+      .expect("content-length", "835");
   });
 });

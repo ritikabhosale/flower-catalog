aa77221b0ecdfeaa6af4cae24925dd41bff3612b
Ritika Bhosale <54773240+ritikabhosale@users.noreply.github.com>
2024-04-27 17:13:46 +0530

    Fix signup route test

--- test/testSignup.js
+++ test/testSignup.js
@@ -1,6 +1,6 @@
-const { createApp } = require('../src/app.js');
-const request = require('supertest');
-const assert = require('assert');
+const { createApp } = require("../src/app.js");
+const request = require("supertest");
+const assert = require("assert");
 
 const mockReadFileSync = (expectedFile, content, expectedEncoding) => {
   return (fileName, encoding) => {
@@ -19,66 +19,81 @@ const mockWriteFileSync = (expectedFile, expectedContent, expectedEncoding) => {
   };
 };
 
-describe('GET /signup', () => {
-  const appConfig = { staticDir: './public' };
+describe("GET /signup", () => {
+  const appConfig = { staticDir: "./public" };
   const fs = {
-    readFile: () => { },
-    readFileSync: mockReadFileSync('./src/app/template/signup.html', 'Signup Template', 'utf8')
+    readFile: () => {},
+    readFileSync: mockReadFileSync(
+      "./src/app/template/signup.html",
+      "Signup Template",
+      "utf8"
+    ),
   };
 
-  it('should serve signup page', (done) => {
-    const req = request(createApp(appConfig, {}, fs, () => { }));
-    req.get('/sign-up')
-      .expect(200, done)
+  it("should serve signup page", (done) => {
+    const req = request(createApp(appConfig, {}, fs, () => {}));
+    req.get("/signup").expect(200, done);
   });
 
-  it('should redirect to home page when already logged in', (done) => {
-    const sessions = { '1': { sessionId: '1', username: 'a@b.c', time: '12' } };
-    const req = request(createApp(appConfig, sessions, {}, () => { }));
-    req.post('/sign-up')
-      .set('Cookie', ['sessionId=1'])
+  it("should redirect to home page when already logged in", (done) => {
+    const sessions = { 1: { sessionId: "1", username: "a@b.c", time: "12" } };
+    const req = request(createApp(appConfig, sessions, {}, () => {}));
+    req
+      .post("/signup")
+      .set("Cookie", ["sessionId=1"])
       .expect(302, done)
-      .expect('location', '/');
+      .expect("location", "/");
   });
 });
 
-describe('POST /sign-up', () => {
-  const appConfig = { staticDir: './public', usersDataPath: './data/userDetails.json' };
+describe("POST /signup", () => {
+  const appConfig = {
+    staticDir: "./public",
+    usersDataPath: "./data/userDetails.json",
+  };
   const users = {
-    'a@g.com': { name: "a", email: "a@g.com", password: "ab", mobNo: "9878" }
+    "a@g.com": { name: "a", email: "a@g.com", password: "ab", mobNo: "9878" },
   };
   const updatedUsers = {
-    'a@g.com': { name: "a", email: "a@g.com", password: "ab", mobNo: "9878" },
-    'r@g.c': { name: "r", email: "r@g.c", password: "r", mobNo: "1" }
+    "a@g.com": { name: "a", email: "a@g.com", password: "ab", mobNo: "9878" },
+    "r@g.c": { name: "r", email: "r@g.c", password: "r", mobNo: "1" },
   };
 
   const fs = {
-    readFileSync: mockReadFileSync('./data/userDetails.json', JSON.stringify(users), 'utf8'),
-    writeFileSync: mockWriteFileSync('./data/userDetails.json', JSON.stringify(updatedUsers), 'utf8')
+    readFileSync: mockReadFileSync(
+      "./data/userDetails.json",
+      JSON.stringify(users),
+      "utf8"
+    ),
+    writeFileSync: mockWriteFileSync(
+      "./data/userDetails.json",
+      JSON.stringify(updatedUsers),
+      "utf8"
+    ),
   };
 
-  it('should persist user details', (done) => {
-    const status = { success: true, message: 'Sign-up Successful' };
-    const req = request(createApp(appConfig, {}, fs, () => { }));
-    req.post('/sign-up')
-      .send('name=r&email=r@g.c&password=r&mobNo=1')
-      .expect(200, JSON.stringify(status), done)
+  it("should persist user details", (done) => {
+    const status = { success: true, message: "Sign-up Successful" };
+    const req = request(createApp(appConfig, {}, fs, () => {}));
+    req
+      .post("/signup")
+      .send("name=r&email=r@g.c&password=r&mobNo=1")
+      .expect(200, JSON.stringify(status), done);
   });
 
-  it('should not allow duplicate username', (done) => {
-    const req = request(createApp(appConfig, {}, fs, () => { }));
-    const status = { success: false, message: 'User already exists' };
-    req.post('/sign-up')
-      .send('name=a&email=a@g.com&password=a&mobNo=9')
+  it("should not allow duplicate username", (done) => {
+    const req = request(createApp(appConfig, {}, fs, () => {}));
+    const status = { success: false, message: "User already exists" };
+    req
+      .post("/signup")
+      .send("name=a&email=a@g.com&password=a&mobNo=9")
       .expect(409, status, done)
-      .expect('content-type', /json/)
+      .expect("content-type", /json/);
   });
 
-  it('should redirect to home page', (done) => {
-    const sessions = { '1': { sessionId: '1', username: 'a@b.c', time: '12' } };
-    const req = request(createApp(appConfig, sessions, fs, () => { }));
-    req.post('/sign-up')
-      .set('Cookie', 'sessionId=1')
-      .expect(302, done)
+  it("should redirect to home page", (done) => {
+    const sessions = { 1: { sessionId: "1", username: "a@b.c", time: "12" } };
+    const req = request(createApp(appConfig, sessions, fs, () => {}));
+    req.post("/signup").set("Cookie", "sessionId=1").expect(302, done);
   });
 });

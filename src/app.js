1b2f9b886612cb2ddfcc59d62b5475660fad05b8
Ritika Bhosale <54773240+ritikabhosale@users.noreply.github.com>
2024-04-27 17:11:29 +0530

    Add signup button on login page

--- src/app.js
+++ src/app.js
@@ -1,32 +1,40 @@
-const express = require('express');
-const { GuestBook } = require('./app/guestBook.js');
-const { logRequest } = require('./app/handlers/logRequest.js');
-const { serveGuestBook, addComment, saveGuestBook } = require('./app/handlers/guestBookHandler.js');
-const { serveComments } = require('./app/handlers/apiHandler.js');
-const { injectCookies } = require('./app/handlers/injectCookies.js');
-const { injectSession } = require('./app/handlers/injectSession.js');
-const { signup, serveSignupForm, saveUserData } = require('./app/handlers/signup.js');
-const { login, serveLoginForm } = require('./app/handlers/loginHandler.js');
-const { logout } = require('./app/handlers/logout.js');
-const { notFound } = require('./app/handlers/notFound.js');
+const express = require("express");
+const { GuestBook } = require("./app/guestBook.js");
+const { logRequest } = require("./app/handlers/logRequest.js");
+const {
+  serveGuestBook,
+  addComment,
+  saveGuestBook,
+} = require("./app/handlers/guestBookHandler.js");
+const { serveComments } = require("./app/handlers/apiHandler.js");
+const { injectCookies } = require("./app/handlers/injectCookies.js");
+const { injectSession } = require("./app/handlers/injectSession.js");
+const {
+  signup,
+  serveSignupForm,
+  saveUserData,
+} = require("./app/handlers/signup.js");
+const { login, serveLoginForm } = require("./app/handlers/loginHandler.js");
+const { logout } = require("./app/handlers/logout.js");
+const { notFound } = require("./app/handlers/notFound.js");
 
-const guestBookTemplate = './src/app/template/guestBook.html';
-const loginFormTemplate = './src/app/template/login.html';
-const signupTemplate = './src/app/template/signup.html';
+const guestBookTemplate = "./src/app/template/guestBook.html";
+const loginFormTemplate = "./src/app/template/login.html";
+const signupTemplate = "./src/app/template/signup.html";
 
 const getUsers = (filePath, fs) => {
   let users = {};
   try {
-    users = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-  } catch (err) { }
+    users = JSON.parse(fs.readFileSync(filePath, "utf8"));
+  } catch (err) {}
   return users;
 };
 
 const getGuestBook = (guestBookPath, fs) => {
   let comments = [];
   try {
-    comments = JSON.parse(fs.readFileSync(guestBookPath, 'utf8'));
-  } catch (err) { }
+    comments = JSON.parse(fs.readFileSync(guestBookPath, "utf8"));
+  } catch (err) {}
   return new GuestBook(comments);
 };
 
@@ -42,19 +50,22 @@ const createApp = (appConfig, sessions, fs, logger) => {
   app.use(injectCookies);
   app.use(injectSession(sessions));
 
-  app.get('/guest-book', serveGuestBook(guestBook, guestBookTemplate, fs));
-  app.post('/guest-book', addComment(guestBook),
-    saveGuestBook(guestBook, guestBookPath, fs));
+  app.get("/guest-book", serveGuestBook(guestBook, guestBookTemplate, fs));
+  app.post(
+    "/guest-book",
+    addComment(guestBook),
+    saveGuestBook(guestBook, guestBookPath, fs)
+  );
 
-  app.get('/api/comments', serveComments(guestBook));
+  app.get("/api/comments", serveComments(guestBook));
 
-  app.get('/login', serveLoginForm(loginFormTemplate, fs));
-  app.post('/login', login(users, sessions));
+  app.get("/login", serveLoginForm(loginFormTemplate, fs));
+  app.post("/login", login(users, sessions));
 
-  app.get('/sign-up', serveSignupForm(signupTemplate, fs));
-  app.post('/sign-up', signup(users), saveUserData(users, usersDataPath, fs));
+  app.get("/signup", serveSignupForm(signupTemplate, fs));
+  app.post("/signup", signup(users), saveUserData(users, usersDataPath, fs));
 
-  app.get('/logout', logout(sessions));
+  app.get("/logout", logout(sessions));
 
   app.use(express.static(staticDir));
   app.use(notFound);
